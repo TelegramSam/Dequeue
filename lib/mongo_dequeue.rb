b48f0b4a3d52f49dc95b39d2700a519f892c5fd8
TelegramSam <telegramsam@gmail.com>
2011-06-24 11:02:51 -0600

    first round of tests

--- lib/mongo_dequeue.rb
+++ lib/mongo_dequeue.rb
@@ -8,10 +8,9 @@ class Mongo::Dequeue
 	attr_reader :connection, :config
 	
 	DEFAULT_CONFIG = {
-		:database   => 'mongo_queue',
-		:collection => 'mongo_queue',
+		:database   => 'mongo_dequeue',
+		:collection => 'mongo_dequeue',
 		:timeout    => 300,
-		:attempts   => 3,
 		:default_priority => 3
 	}.freeze
 	
@@ -38,7 +37,7 @@ class Mongo::Dequeue
 	# Example:
 	#    queue.insert(:name => 'Billy', :email => 'billy@example.com', :message => 'Here is the thing you asked for')
 	def push(body, item_opts = {})
-		dup_key = Mongo::Dequeue.generate_duplicate_key(body)
+		dup_key = item_opts[:duplicate_key] || Mongo::Dequeue.generate_duplicate_key(body)
 		
 		selector = {
 			:duplicate_key => dup_key,
@@ -64,20 +63,47 @@ class Mongo::Dequeue
 	# Lock and return the next queue message if one is available. Returns nil if none are available. Be sure to
 	# review the README.rdoc regarding proper usage of the locking process identifier (locked_by).
 	# Example:
-	#    doc = queue.pop(Thread.current.object_id)
+	#    doc = queue.pop()
+	
+	# {:body=>"foo", :id=>"4e039c372b70275e345206e4"}
 
 	def pop(opts = {})
-		timeout = opts[:timeout] || @config[:timeout]
-		cmd = OrderedHash.new
-	    cmd['findandmodify'] = @config[:collection]
-	    cmd['update']        = {'$set' => {:locked_till => Time.now.utc+timeout}} 
-	    cmd['query']         = {:complete => false, '$or'=>[{:locked_till=> nil},{:locked_till=>{'$lt'=>Time.now.utc}}] }
-	    cmd['sort']          = {:priority=>-1,:inserted_at=>-1}
-	    cmd['limit']         = 1
-	    cmd['new']           = true
-	    value_of collection.db.command(cmd)
+		begin
+			timeout = opts[:timeout] || @config[:timeout]
+			cmd = BSON::OrderedHash.new
+		    cmd['findandmodify'] = @config[:collection]
+		    cmd['update']        = {'$set' => {:locked_till => Time.now.utc+timeout}} 
+		    cmd['query']         = {:complete => false, '$or'=>[{:locked_till=> nil},{:locked_till=>{'$lt'=>Time.now.utc}}] }
+		    cmd['sort']          = {:priority=>-1,:inserted_at=>1}
+		    cmd['limit']         = 1
+		    cmd['new']           = true
+		    result = collection.db.command(cmd)
+		rescue Mongo::OperationFailure => of
+			return nil
+		end
+	    return {
+	    	:body => result['value']['body'],
+	    	:id => result['value']['_id'].to_s
+	    }
 	end
 	
+	
+	# Remove the document from the queue. This should be called when the work is done and the document is no longer needed.
+	# You must provide the process identifier that the document was locked with to complete it.
+	def complete(id)
+		cmd = BSON::OrderedHash.new
+		cmd['findandmodify'] = @config[:collection]
+		cmd['query']         = {:_id => BSON::ObjectId.from_string(id), :complete => false}
+		cmd['update']        = {:completed_at => Time.now.utc, :complete => true}
+		cmd['limit']         = 1
+		collection.db.command(cmd)
+	end
+	
+	# Removes completed job history
+	def cleanup()
+		collection.remove({:complete=>true});
+	end
+  	
 	# Provides some information about what is in the queue. We are using an eval to ensure that a
 	# lock is obtained during the execution of this query so that the results are not skewed.
 	# please be aware that it will lock the database during the execution, so avoid using it too
@@ -86,31 +112,35 @@ class Mongo::Dequeue
     	js = "function queue_stat(){
 			      return db.eval(
 			      function(){
-			        var a = db.#{config[:collection]}.count({'locked_by': null, 'attempts': {$lt: #{config[:attempts]}}});
-			        var l = db.#{config[:collection]}.count({'locked_by': /.*/});
-			        var e = db.#{config[:collection]}.count({'attempts': {$gte: #{config[:attempts]}}});
+			      	var nowutc = new Date();
+			      	var a = db.#{config[:collection]}.count({'complete': false, '$or':[{'locked_till':null},{'locked_till':{'$lt':nowutc}}] });
+			        var c = db.#{config[:collection]}.count({'complete': true});
 			        var t = db.#{config[:collection]}.count();
-			        return [a, l, e, t];
+			        var l = db.#{config[:collection]}.count({'complete': false, 'locked_till': {'$gte':nowutc} });
+			        return [a, c, t, l];
 			      }
 			    );
 			  }"
-		available, locked, errors, total = collection.db.eval(js)
+		available, complete, total, locked = collection.db.eval(js) 
 		{ :locked    => locked.to_i,
-		  :errors    => errors.to_i,
+		  :complete => complete.to_i,
 		  :available => available.to_i,
 		  :total     => total.to_i }
 	end
 	
 	def self.generate_duplicate_key(body)
-		begin
-			return Digest::MD5.hexdigest(JSON.generate(body))
-		rescue
-			return body
-		end
+		return Digest::MD5.hexdigest(body) if body.class == "String"
+		return Digest::MD5.hexdigest(body) if body.class == "Fixnum"
+		#else	
+		return Digest::MD5.hexdigest(body.to_json) #won't ever match a duplicate. Need a better way to handle hashes and arrays.
 	end
 	
 	
 	protected
+	
+	def value_of(result) #:nodoc:
+		result['okay'] == 0 ? nil : result['value']
+	end
 	
 	def collection #:nodoc:
 		@connection.db(@config[:database]).collection(@config[:collection])
--- spec/mongo_dequeue_spec.rb
+++ spec/mongo_dequeue_spec.rb
@@ -13,7 +13,6 @@ describe Mongo::Dequeue do
     opts   = {
       :database   => 'mongo_queue_spec',
       :collection => 'spec',
-      :attempts   => 4,
       :timeout    => 60}
     @db = Mongo::Connection.new('localhost', nil, :pool_size => 4)
     @queue = Mongo::Dequeue.new(@db, opts)
@@ -37,18 +36,13 @@ describe Mongo::Dequeue do
       @queue.config[:collection].should eql('spec')
     end
 
-    it "should allow attempts option" do
-      @queue.config[:attempts].should eql(4)
-    end
-  
     it "should allow timeout option" do
       @queue.config[:timeout].should eql(60)
     end
   
     it "should have a sane set of defaults" do
       q = Mongo::Dequeue.new(nil)
-      q.config[:collection].should eql 'mongo_queue'
-      q.config[:attempts].should   eql 3
+      q.config[:collection].should eql 'mongo_dequeue'
       q.config[:timeout].should    eql 300
     end
   end
@@ -115,19 +109,48 @@ describe Mongo::Dequeue do
 	       @queue.flush!
 	    end
 	    
-	    it "should combine identical bodies" do
+	    it "should combine identical bodies of type string" do
 	    	a = insert_and_inspect("foo")
 		    b = insert_and_inspect("foo")
 		    @queue.send(:collection).count.should be 1  
-			
 	    end
 
-	    it "should not combine different bodies" do
+	    it "should not combine different bodies of type string" do
 	    	a = insert_and_inspect("foo")
 		    b = insert_and_inspect("bar")
 		    @queue.send(:collection).count.should be 2
 		    b['count'].should be 1
 	    end
+	    
+	    it "should combine identical bodies of type struct" do
+	    	pending "Test after we have a better way of handling structs"
+	    	a = insert_and_inspect({:a=>'a',:b=>'b'})
+		    b = insert_and_inspect({:a=>'a',:b=>'b'})
+		    c = insert_and_inspect({:b=>'b',:a=>'a'})
+		    @queue.send(:collection).count.should be 1
+	    end
+
+	    it "should not combine different bodies of type struct" do
+	    	a = insert_and_inspect({:a=>'a',:b=>'b'})
+		    b = insert_and_inspect({:a=>'a',:c=>'c'})
+		    @queue.send(:collection).count.should be 2
+		    b['count'].should be 1
+	    end
+
+	    it "should combine based on duplication_key" do
+	    	a = insert_and_inspect({:a=>'a',:b=>'b'}, :duplicate_key => 'match')
+		    b = insert_and_inspect({:a=>'a',:c=>'c'}, :duplicate_key => 'match')
+		    @queue.send(:collection).count.should be 1
+		    b['count'].should be 2
+	    end
+	    
+	    it "should not combine based on duplication_key" do
+	    	a = insert_and_inspect("foo", :duplicate_key => 'match')
+		    b = insert_and_inspect("foo", :duplicate_key => 'nomatch')
+		    @queue.send(:collection).count.should be 2
+		    b['count'].should be 1
+	    end
+	    
 	end
 
 	describe "Popping messages" do
@@ -138,12 +161,95 @@ describe Mongo::Dequeue do
 	    it "should return message" do
 	    	a = insert_and_inspect("foo")
 		    m = @queue.pop
-		    pp m
-		    m['body'].should eq "foo"
+		    m[:body].should eq "foo"
 		    @queue.send(:collection).count.should be 1  
-			
+	    end
+	    
+	    it "should return an id" do
+	    	a = insert_and_inspect("foo")
+		    m = @queue.pop
+		    m[:id].should_not be nil
+		    @queue.send(:collection).count.should be 1  
 	    end
 
+	    it "should return nil when queue is empty" do
+	    	m = @queue.pop
+		    m.should be nil
+		    @queue.send(:collection).count.should be 0 
+	    end
+	    
+	    it "should complete ok" do
+	    	a = insert_and_inspect("foo")
+		    m = @queue.pop
+		    @queue.complete(m[:id])
+		    m2 = @queue.pop
+		    m2.should be nil
+		    @queue.send(:collection).count.should be 1
+	    end
+	    
+	    it "should pop again after timeout" do
+	    	a = insert_and_inspect("foo")
+		    m = @queue.pop(:timeout => 1)
+		    sleep(2)
+		    m2 = @queue.pop
+		    m2[:id].should eq m[:id]
+		    @queue.send(:collection).count.should be 1
+	    end
+	    
+	    it "should pop in order" do
+	       @a = insert_and_inspect("a")
+	       @b = insert_and_inspect("b")
+	       @c = insert_and_inspect("c")
+	       @d = insert_and_inspect("d")
+	       
+	       @ap = @queue.pop
+	       @bp = @queue.pop
+	       @cp = @queue.pop
+	       @dp = @queue.pop
+	       
+	       @ap[:body].should eq "a"
+	       @bp[:body].should eq "b"
+	       @cp[:body].should eq "c"
+	       @dp[:body].should eq "d"
+	    end
+	    
+	end
+
+	describe "Stats" do
+	    before(:all) do
+	       @queue.flush!
+	       @a = insert_and_inspect("a")
+
+	       @b = insert_and_inspect("b")
+	       @c = insert_and_inspect("c")
+	       @d = insert_and_inspect("d")
+	       @e = insert_and_inspect("e")
+	       
+	       @ap = @queue.pop(:timeout => 1)
+	       @bp = @queue.pop
+	       @cp = @queue.pop
+	       
+	       sleep(2)
+	       
+	       @queue.complete(@bp[:id])
+	       
+	       @stats = @queue.stats
+	    end
+	    #locked, complete, available, total
+	    it "should count complete" do
+	    	@stats[:complete].should == 1
+	    end
+	    it "should count total" do
+	    	@stats[:total].should == 5
+	    end
+	    it "should count available" do
+	    	@stats[:available].should == 3
+	    end
+	    it "should count locked" do
+	    	@stats[:locked].should == 1
+	    end
+
+	    
 	end
 
     
